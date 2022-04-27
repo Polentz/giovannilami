d7698f74e062b75565551cc04a40a5bebd190fb5
Giulia Polenta <giuliapolenta@gmail.com>
2022-04-27 18:20:14 +0200

    smooth scroll into view

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-barba.init();
+// import barba from '@barba/core';
+// import barbaCss from '@barba/css';
+
+// barba.use(barbaCss);
+
+barba.init({
+    transitions: [
+        {
+            name: "fade",
+            once() { }
+        }
+    ]
+});
+
+barba.hooks.enter(() => {
+    const element = document.querySelector(".content__wrapper");
+    console.log(element);
+    element.scrollIntoView({ behavior: "smooth" });
+});
+
+// const link = document.querySelector(".ui__top a");
+// link.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     const href = link.getAttribute("href");
+//     document.querySelector(href).scrollIntoView({
+//         behavior: "smooth"
+//     });
+// });
+
 
 // hooks
+// once
 // data.trigger link
 // data.current current page
 // data.next next page
