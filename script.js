c3229ae19831c7002171fec32a63e92b4807b75c
Giulia Polenta <giuliapolenta@gmail.com>
2022-04-27 10:54:42 +0200

    test barba

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+barba.use(barbaCss);
+
 barba.init({
-    // ...
+    transitions: [
+        {
+            once() { }
+
+        }
+    ]
 });
