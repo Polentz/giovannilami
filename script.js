4786622e6d643cb5275078788a2fe6ab8ac4f705
Giulia Polenta <giuliapolenta@gmail.com>
2022-04-27 11:52:19 +0200

    test barba 2

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 barba.use(barbaCss)
 
-const bodyTag = document.querySelector("body");
-const mainTag = document.querySelector("main");
+// const bodyTag = document.querySelector("body");
+// const mainTag = document.querySelector("main");
 
 barba.init({
     debug: true,
     transitions: [
         {
-            name: "fade",
-            once() { },
-            beforeEnter({ }) {
+            // name: "fade",
+            // once() { },
+            // beforeEnter({ }) {
 
-                window.scrollTo({
-                    top: 200 + "vh",
-                    behavior: "smooth"
-                })
-            }
+            //     window.scrollTo({
+            //         top: 200 + "vh",
+            //         behavior: "smooth"
+            //     })
+            // }
+            once() { }
         }
     ]
 })
